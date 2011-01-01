c95a9f3f08787176498ea02293aafd07acf3a403
Kent Fredric <kentfredric@gmail.com>
2011-01-01 22:02:54 +1300

    +documentation

--- lib/Paludis/ResumeState/Serialization/Basic.pm
+++ lib/Paludis/ResumeState/Serialization/Basic.pm
@@ -3,17 +3,23 @@ use warnings;
 
 package Paludis::ResumeState::Serialization::Basic;
 
-# ABSTRACT: Basic & Consistent ResumeState serialization interface.
+# ABSTRACT: Basic & Consistent Resume-State serialization interface.
 
 use Paludis::ResumeState::Serialization::Grammar;
 use Params::Util qw( _HASHLIKE _STRING );
+use Class::Load 0.06 qw();
+use English qw( -no_match_vars );
 
-use Data::Dumper qw( Dumper );
-
+## no critic ( RequireArgUnpacking ProhibitUnreachableCode ProhibitMagicNumbers  RequireCheckedSyscalls )
 sub _debug {
-    local $Data::Dumper::Indent=1;
-    local $Data::Dumper::Maxdepth=3;
-    # print Dumper( \@_ );
+
+  return;    # Comment this for tracing.
+  return unless Class::Load::load_optional_class('Data::Dumper');
+
+  local $Data::Dumper::Indent   = 1;
+  local $Data::Dumper::Maxdepth = 3;
+  print Data::Dumper::Dumper( \@_ );
+  return 1;
 }
 
 sub serialize {
@@ -21,7 +27,6 @@ sub serialize {
   my $string = _serialize_basic_resumespec($data);
   return $string;
 }
-
 
 sub deserialize {
   my ( $self, $content ) = @_;
@@ -35,34 +40,37 @@ sub deserialize {
     my ($parameters) = @_;
     return { type => 'array', parameters => $parameters, count => scalar @{$parameters} };
   };
-  local %/;
-  if ( $content =~ Paludis::ResumeState::Serialization::Grammar::grammar() ) {
-    my $result = \%/;
-    return $result;
+  my $results;
+  my $grammar = Paludis::ResumeState::Serialization::Grammar::grammar();
+  local (%RS) = ();
+
+  if ( $content =~ $grammar ) {
+    $results = \%RS;
+    return $results;
   }
 
   return;
 }
 
-
 sub _serialize_basic_array {
-  my $data = shift;
-  my $out = "c(";
-  my $i = 1;
+  my $data  = shift;
+  my $out   = 'c(';
+  my $i     = 1;
   my @pairs = ();
-  for my $value ( @{ $data->{parameters} } ){
+  for my $value ( @{ $data->{parameters} } ) {
     my $key = $i;
     $i++;
     push @pairs, sprintf q{%s=%s;}, $key, _serialize_basic_value($value);
   }
-  push @pairs , 'count=' . _serialize_basic_value( $data->{count} ) . ';';
+  push @pairs, 'count=' . _serialize_basic_value( $data->{count} ) . q{;};
   $out .= ( join q{}, @pairs );
   $out .= ')';
   return $out;
 }
+
 sub _serialize_basic_class {
   my $data = shift;
-  my $out = $data->{classname} . "(";
+  my $out  = $data->{classname} . '(';
   $out .= _serialize_basic_parameters( $data->{parameters} );
   $out .= ')';
   return $out;
@@ -71,15 +79,15 @@ sub _serialize_basic_class {
 sub _serialize_basic_value {
   my $value = shift;
   if ( defined _STRING($value) ) {
-    $value =~ s/"/\\"/g;
+    $value =~ s/"/\\"/gmsx;
     return sprintf q{"%s"}, $value;
   }
   if ( _HASHLIKE($value) ) {
     if ( defined $value->{type} and $value->{type} eq 'class' ) {
-      return _serialize_basic_class( $value );
+      return _serialize_basic_class($value);
     }
     if ( defined $value->{type} and $value->{type} eq 'array' ) {
-      return _serialize_basic_array( $value );
+      return _serialize_basic_array($value);
     }
     Carp::croak("UNEXPECTED PARAMETER TYPE: $value->{type} ");
 
@@ -89,13 +97,14 @@ sub _serialize_basic_value {
 }
 
 sub _serialize_basic_parameters {
-  my $data  = shift;
+  my $data = shift;
   _debug( 'serialize_basic_parameters', $data );
 
   my @pairs = ();
-  for my $record ( @$data ) {
-    my ( $key, $value ) = @{ $record };
-#    my $value = $data->{$key};
+  for my $kv_pair ( @{$data} ) {
+    my ( $key, $value ) = @{$kv_pair};
+
+    #    my $value = $data->{$key};
     push @pairs, sprintf q{%s=%s;}, $key, _serialize_basic_value($value);
   }
   return ( join q{}, @pairs );
@@ -104,19 +113,18 @@ sub _serialize_basic_parameters {
 sub _serialize_basic_resumespec {
   my $data = shift;
   _debug( 'serialize_basic_resumespec', $data );
-  return undef unless _HASHLIKE($data);
-  return undef unless defined $data->{ResumeSpec};
-  return undef unless defined $data->{ResumeSpec}->{classname};
-  return undef unless defined $data->{ResumeSpec}->{type} and $data->{ResumeSpec}->{type} eq 'class';
-  return undef unless defined $data->{ResumeSpec}->{parameters};
-  return undef unless defined $data->{ResumeSpec}->{pid};
-  my $out = $data->{ResumeSpec}->{classname} . '@' . $data->{ResumeSpec}->{pid};
+  return unless _HASHLIKE($data);
+  return unless defined $data->{ResumeSpec};
+  return unless defined $data->{ResumeSpec}->{classname};
+  return unless defined $data->{ResumeSpec}->{type} and $data->{ResumeSpec}->{type} eq 'class';
+  return unless defined $data->{ResumeSpec}->{parameters};
+  return unless defined $data->{ResumeSpec}->{pid};
+  my $out = $data->{ResumeSpec}->{classname} . q{@} . $data->{ResumeSpec}->{pid};
   $out .= '(';
   $out .= _serialize_basic_parameters( $data->{ResumeSpec}->{parameters} );
   $out .= ');';
   return $out;
 }
-
 
 1;
 
