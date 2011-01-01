188a859a2bb4e4e3d51819c3c75e8ffa2c5e3050
Kent Fredric <kentfredric@gmail.com>
2011-01-01 23:06:08 +1300

    +ABSTRACT

--- lib/Paludis/ResumeState/Serialization.pm
+++ lib/Paludis/ResumeState/Serialization.pm
@@ -12,10 +12,32 @@ use Carp qw();
 
 no autovivification;
 
+sub _get_content_fh {
+  my ($config) = shift;
+  Carp::croak("{ filehandle => } must be a HANDLE ")
+    unless _HANDLE( $config->{filehandle} );
+  local $/ = undef;
+  my $fh = $config->{filehandle};
+  return scalar <$fh>;
+}
+
+sub _get_content_filename {
+  my ($config) = shift;
+  Carp::croak("{ filename => } must be a STRING")
+    unless _STRING( $config->{filename} );
+  my $fn = $config->{filename};
+  my $fh;
+  Carp::croak("Can't open $fn for read, $!")
+    unless open $fh, '<', $fn;
+  local $/ = undef;
+  return scalar <$fh>;
+}
+
 sub _get_content {
   my ($config) = shift;
   return $config->{content} if _STRING( $config->{content} );
-  Carp::croak('{ content => } must be a scalar ')
+  return ${ $config->{content} } if _SCALAR( $config->{content} );
+  Carp::croak('{ content => } must be a scalar or a scalar ref')
     if defined $config->{content};
 
   return;
@@ -90,7 +112,16 @@ sub deserialize {
   my $content = _get_content($config);
 
   Carp::croak('Can\'t deserialize, no content provided, provide deserialize( hash ) with content => ')
+  $content = _get_content_fh($config) if ( not defined $content and defined $config->{filehandle} );
+
+  $content = _get_content_file($config) if ( not defined $content and defined $config->{filename} );
+
+  Carp::croak(
+    "Can't deserialize, no content provided, provide deserialize(\$hash) with content => , filehandle => , or filename =>")
     unless defined $content;
+
+  Carp::croak("No {format=> } specified, pick either 'basic', 'simple_objects', or 'full_objects'")
+    unless defined $config->{format};
 
   return _serializer( $config->{format} )->deserialize( $config->{content} );
 }
